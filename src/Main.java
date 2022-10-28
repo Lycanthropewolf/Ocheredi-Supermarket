a2d3dd0708fd13f365b350253f853e4af82806c2
Антон <antonolegovich2017@icloud.com>
2022-10-28 19:09:47 +0600

    sd

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) {
+        Queue<String> queue1= new ArrayDeque<>();
+        Queue<String> queue2= new ArrayDeque<>();
+    }
+}
